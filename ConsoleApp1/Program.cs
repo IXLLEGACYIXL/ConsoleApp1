8a814c2eaa1b00f864d16bf39454e3b82f8c18d6
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-04-02 17:52:36 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Homp;
+using NexVYaml;
+using NexVYaml.Emitter;
+using NexVYaml.Serialization;
+using Stride.CommunityToolkit.Engine;
+using Stride.Core;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+var x = new T<int>();
+var s = YamlSerializer.SerializeToString(x);
+var d = YamlSerializer.Deserialize<T<int>>(s);
+namespace Homp
+{
+    [DataContract]
+    class T<X>
+    {
+        public X S;
+        public bool B => typeof(X).IsArray;
+    }
+}
