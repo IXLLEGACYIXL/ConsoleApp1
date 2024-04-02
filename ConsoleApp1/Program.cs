a55ba5cc731888f6779302e3a51186732ce54c76
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-04-02 17:57:31 +0200

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,12 +12,22 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 var x = new T<int>();
+var options = new YamlSerializerOptions()
+{
+    SecureMode = true
+};
 var s = YamlSerializer.SerializeToString(x);
 var d = YamlSerializer.Deserialize<T<int>>(s);
+var throws = YamlSerializer.SerializeToString<Bob>(d,options);
+var throws2 = YamlSerializer.Deserialize<T<int>>(throws);
 namespace Homp
 {
+    interface Bob
+    {
+
+    }
     [DataContract]
-    class T<X>
+    class T<X> : Bob
     {
         public X S;
         public bool B => typeof(X).IsArray;
